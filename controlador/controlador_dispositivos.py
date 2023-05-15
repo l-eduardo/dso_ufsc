12a40f38577e933fbbf470e7a47341eba31c4b50
Luiz Eduardo da Silva <luiz.silva@srssistemas.com>
2023-05-15 01:50:12 -0300

    esqueleto dispositivo controlador

---
+++ controlador/controlador_dispositivos.py
@@ -0,0 +1,17 @@
+
+
+class ControladorDispositivos():
+    def __init__(self):
+        
+    def lista_dispositivos(self):
+        pass
+
+    def filtra_por_patrimonio(self, patrimonio):
+        pass
+
+    def abre_tela()
+        pass
+
+    def retorna()
+        pass
+    
