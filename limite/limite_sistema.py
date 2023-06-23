bc6225df1c816243f3eac388ee13d7b95be031f3
Luiz Eduardo da Silva <luiz.silva@srssistemas.com>
2023-06-23 12:18:08 -0300

    Formatando limite sistema

--- limite/limite_sistema.py
+++ limite/limite_sistema.py
@@ -11,15 +11,20 @@ class LimiteSistema:
             return button
 
         def init_components(self):
-            buttons = [[sg.Button('Dispositivos', key=1, pad=(0, (10, 5)))],
-                       [sg.Button('Emprestimos', key=2, pad=(0, 5))],
-                       [sg.Button('Funcionarios', key=3, pad=(0, 5))],
-                       [sg.Button('Departamentos', key=4, pad=(0, 5))],
-                       [sg.Button('Sair', key=0, pad=(0, (50, 10)))],
+            buttons = [[sg.Button('Dispositivos', key=1, pad=(0, (10, 5)), expand_x=True)],
+                       [sg.Button('Emprestimos', key=2, pad=(0, 5), expand_x=True)],
+                       [sg.Button('Funcionarios', key=3, pad=(0, 5), expand_x=True)],
+                       [sg.Button('Departamentos', key=4, pad=(0, 5), expand_x=True)],
+                       [sg.Button('Sair', key=0, pad=(0, (50, 10)), expand_x=True)],
                        ]
 
             layout = [
-                [sg.Text('Menu princpal', font=("Roboto",25), justification="center", tooltip="Menu principal")],
-                [sg.Column(buttons, justification="center", element_justification="center")],
+                [sg.Text('Menu princpal',
+                         font=("Roboto",25),
+                         justification="center",
+                         tooltip="Menu principal")],
+                [sg.Column(buttons,
+                         justification="center",
+                         element_justification="center")],
             ]
             self.__window = sg.Window('Sistema de emprestimos').Layout(layout)
