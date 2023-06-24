0c46f091230d06dbad381dc2123090fd4eeadab1
Luiz Eduardo da Silva <luiz.silva@srssistemas.com>
2023-06-24 20:53:59 -0300

    tirando opcao que usa digito zero (retornar)

--- controlador/controlador_sistema.py
+++ controlador/controlador_sistema.py
@@ -14,18 +14,18 @@ class ControladorSistema:
 
     def abre_tela(self):
         lista_opt = {
-            0: self.sair,
             1: self.dispositivos,
             2: self.emprestimos,
             3: self.funcionarios,
             4: self.departamentos,
+            5: self.sair,
         }
 
         while True:
             lista_opt[self.__limite.tela_opcoes()]()
 
     def sair(self):
-        exit(0);
+        exit(0)
 
     def dispositivos(self):
         self.__controlador_computadores.abre_tela()
